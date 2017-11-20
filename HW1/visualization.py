73efdd61ac76f2f6ff1b650438b24b095d0b90cb
Abdel K. Bokharouss <a.k.bokharouss@student.tue.nl>
2017-11-20 11:28:40 +0100

    file for 1.1 visualization

---
+++ HW1/visualization.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 20 11:08:50 2017
+@author: Abdel K. Bokharouss
+"""
+# 1.1 Visualization - Matrix with ages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+speed_date = pd.read_csv("speed_dating_assignment.csv")
