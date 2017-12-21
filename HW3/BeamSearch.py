d05cb5ffd1fe0c83471d2ea9799f52c85147dfb3
Joris Rombouts <j.c.rombouts@student.tue.nl>
2017-12-21 15:05:55 +0100

    new structure

--- HW3/BeamSearch.py
+++ HW3/BeamSearch.py
@@ -1,33 +1,61 @@
-from queue import Queue, PriorityQueue
+import heapq
+from heapq import heappush, heappop
 
-# Beam width
-w = 0
+class priority_queue:
+    def __init__(self, max_size):
+        self.items = []
+        self.max = max_size
+        self.index = 0
 
-# Beam depth
-d = 0
+    def push(self, item, priority):
+        #heapq.heappush(self.items, (-priority, x))
+        if len(self.items) < self.max:
+            heapq.heappush(self.items, (-priority, self.index, item))
+            self.index += 1
+        else:
+        # Equivalent to a push, then a pop, but faster
+        #spilled_value =
+            heapq.heappushpop(self.items, (-priority, self.index, item))
+            self.index += 1
 
-# Result set size
-q = 0
+    def pop(self):
+        return heapq.heappop(self.items)[-1]
+
+    def empty(self):
+        return not self.items
+
+    def print_elements(self):
+        result = []
+        for i in self.items:
+            result.append(i)
+        return result
 
 
-def beam_search():
-    candidate_queue = Queue()
-    result_set = PriorityQueue(q)
-    for level in range(1, d):
-        beam = PriorityQueue(w)
-        while candidate_queue.not_empty:
-            seed = candidate_queue.get()
-            set = n(seed)
-            for desc in set:
-                quality = phi(desc)
+def phiSet(desc):
+
+def phiYule(Set):
+
+def constraints(descList):
+    return len(descList) > 20
+
+def phiEntropy(Set):
+
+def beam_search(d, w, q, qualityFunc, satisfies, refinement, desclist):
+    candidateQueue = []
+    resultSet = priorityMax(q)
+    for level in range(0, d):
+        beam = priorityMax(w)
+        while candidateQueue.not_empty:
+            seed = candidateQueue.get()
+            set_refined = refinement(seed)
+            for desc in set_refined:
+                quality = phiYule(desc)
                 if desc.satisfiesAll(C):
-                    result_set.put(desc.quality)
+                    resultSet.put(desc.quality)
                     beam.put(desc.quality)
             while beam.not_empty:
-                candidate_queue.put(beam.get())
+                candidateQueue.put(beam.get())
+    return resultSet
 
-def n():
-
-def phi():
-
-
+result = beam_search(d=2, w=5, q=5, qualityFunc = phiYule, satisfies = constraints, refinement, df)
+result
